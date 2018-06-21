f3798c1df3b0d3b8ab1d825e66cd6e5275621a55
Sebastian E. Kwiatkowski <sebastian@aisummary.com>
2018-06-21 03:29:21 +0200

    Added Java sample code Microsoft Cognitive Services STT

---
+++ speech-recognition/microsoft-cognitive-services-speech-to-text/MicrosoftCognitiveServicesSpeechToText.java
@@ -0,0 +1,24 @@
+// input is a string holding the path to a WAV file.
+
+Path inputPath = Paths.get(input);
+byte[] data = Files.readAllBytes(inputPath);
+
+String tokenHost = "https://westus.api.cognitive.microsoft.com";
+String tokenPath = "/sts/v1.0/issueToken";
+String tokenUrl = tokenHost.concat(tokenPath);
+
+String token = Unirest.post(tokenUrl)
+    .header("Ocp-Apim-Subscription-Key", subscriptionKey)
+    .asString()
+    .getBody();
+
+String sttHost = "https://westus.stt.speech.microsoft.com";
+String sstPath = "/speech/recognition/conversation/cognitiveservices/v1?language=en-US";
+String sstUrl = sttHost.concat(sstPath);
+
+String response = Unirest.post(sstUrl)
+    .header("Content-Type", "application/json")
+    .header("Authorization", token)
+    .body(data)
+    .asString()
+    .getBody();
