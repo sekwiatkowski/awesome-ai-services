41e62de57fcb46836641dfc2e5b42793138e25d4
Sebastian E. Kwiatkowski <sebastian@aisummary.com>
2018-06-21 05:09:15 +0200

    Added Java sample code for entity recognition with MS Text Analytics

---
+++ entity-recognition/microsoft-cognitive-services-text-analytics/MicrosoftCognitiveServicesTextAnalyticsEntityRecognition.java
@@ -0,0 +1,69 @@
+public class Document {
+    public String id, language, text;
+
+    public Document(String id, String language, String text){
+        this.id = id;
+        this.language = language;
+        this.text = text;
+    }
+}
+
+public class Documents {
+    public List<Document> documents;
+
+    public Documents() {
+        this.documents = new ArrayList<>();
+    }
+    public void add(String id, String language, String text) {
+        this.documents.add (new Document (id, language, text));
+    }
+}
+
+public class Entity {
+    public String name;
+    public String wikipediaLanguage;
+    public String wikipediaId;
+    public String wikipediaUrl;
+    public String bingId;
+
+    public Entity(String name, String wikipediaLanguage, String wikipediaId, String wikipediaUrl, String bingId) {
+        this.name = name;
+        this.wikipediaLanguage = wikipediaLanguage;
+        this.wikipediaId = wikipediaId;
+        this.wikipediaUrl = wikipediaUrl;
+        this.bingId = bingId;
+    }
+}
+
+public class EntityRecognition {
+    public String id;
+    public List<Entity> entities;
+
+    public EntityRecognition(String id, List<Entity> entities) {
+        this.id = id;
+        this.entities = entities;
+    }
+}
+
+public class EntityRecognitionResponse {
+    public List<EntityRecognition> documents;
+}
+
+String host = "https://westeurope.api.cognitive.microsoft.com";
+String path = "/text/analytics/v2.0/entities";
+String url = host.concat(path);
+
+Documents documents = new Documents();
+documents.add(id, language, text);
+
+Gson gson = new Gson();
+String body = gson.toJson(documents);
+
+String stringResponse = Unirest.post(url)
+    .header("Content-Type", "application/json")
+    .header("Ocp-Apim-Subscription-Key", subscriptionKey)
+    .body(body)
+    .asString()
+    .getBody();
+
+Response response = gson.fromJson(stringResponse, EntityRecognitionResponse.class);
