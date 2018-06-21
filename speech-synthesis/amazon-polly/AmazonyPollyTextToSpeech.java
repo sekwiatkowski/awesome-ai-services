f9c4d926d0c86a6021474967bd8e4985dfe2d398
Sebastian E. Kwiatkowski <sebastian@aisummary.com>
2018-06-21 03:57:45 +0200

    Added Java sample code for Amazon Polly

---
+++ speech-synthesis/amazon-polly/AmazonyPollyTextToSpeech.java
@@ -0,0 +1,25 @@
+// "ssml" is the input provided in the Speech Synthesis Markup Language.
+// Alternatively, you can provide raw text and set the text type to "TextType.Text"
+
+AmazonPolly client = AmazonPollyClientBuilder
+    .standard()
+    .withCredentials(credentialsProvider)
+    .withRegion(Regions.US_EAST_1)
+    .build();
+
+SynthesizeSpeechRequest request = new SynthesizeSpeechRequest()
+    .withText(ssml)
+    .withTextType(TextType.Ssml)
+    .withVoiceId(VoiceId.Matthew)
+    .withOutputFormat(OutputFormat.Mp3);
+
+SynthesizeSpeechResult result = client.synthesizeSpeech(request);
+
+InputStream audioStream = result.getAudioStream();
+
+FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+
+IOUtils.copy(audioStream, fileOutputStream);
+
+audioStream.close();
+fileOutputStream.close();
