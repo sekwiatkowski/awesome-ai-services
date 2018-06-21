2c48af26dfb77e4863a9f78eb94ecf976ecb707f
Sebastian E. Kwiatkowski <sebastian@aisummary.com>
2018-06-21 03:18:11 +0200

    Added Java sample code for Amazon Transcribe

---
+++ speech-recognition/amazon-transcribe/AmazonTranscribeSpeechToText.java
@@ -0,0 +1,43 @@
+// jobName is a self-descriptive string.
+// For speech in US English, languageCode is set to "en-US".
+// s3Uri is a string pointing to an audio file stored in an S3 bucket.
+
+AmazonTranscribe client = AmazonTranscribeClientBuilder
+    .standard()
+    .withCredentials(credentialsProvider)
+    .withRegion(Regions.US_EAST_1)
+    .build();
+
+StartTranscriptionJobRequest startRequest = new StartTranscriptionJobRequest();
+
+startRequest.setTranscriptionJobName(jobName);
+startRequest.setLanguageCode(languageCode);
+
+Media media = new Media();
+media.setMediaFileUri(s3Uri);
+startRequest.setMedia(media);
+startRequest.setMediaFormat(MediaFormat.Mp3.toString());
+
+client.startTranscriptionJob(startRequest);
+
+GetTranscriptionJobRequest getRequest = new GetTranscriptionJobRequest();
+getRequest.setTranscriptionJobName(jobName);
+
+while (true) {
+    GetTranscriptionJobResult getResponse = client.getTranscriptionJob(getRequest);
+
+    TranscriptionJob job = getResponse.getTranscriptionJob();
+
+    if (job.getTranscriptionJobStatus().equals(TranscriptionJobStatus.COMPLETED.toString())) {
+
+        Transcript transcript = job.getTranscript();
+
+        String transcriptUri = transcript.getTranscriptFileUri();
+        System.out.println(transcriptUri);
+
+        break;
+    }
+    else {
+        Thread.sleep(1000L);
+    }
+}
