bf39eb7592d171bdd5c4406a89449ec7744cc1cb
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-22 22:44:14 +0200

    Added Java sample code for MT w/ Amazon Translate

---
+++ machine-translation/amazon-translate/AmazonTranslateMachineTranslation.java
@@ -0,0 +1,18 @@
+// "input" is a string holding the source document.
+// To translate from English to German, set "sourceLanguageCode" to "en" and "targetLanguageCode" to "de".
+
+AmazonTranslate client = AmazonTranslateClientBuilder
+    .standard()
+    .withCredentials(credentialsProvider)
+    .withRegion(Regions.US_EAST_1)
+    .build();
+
+TranslateTextRequest request = new TranslateTextRequest()
+    .withText(input)
+    .withSourceLanguageCode(sourceLanguageCode)
+    .withTargetLanguageCode(targetLanguageCode);
+
+TranslateTextResult result = client.translateText(request);
+String translation = result.getTranslatedText();
+
+System.out.println(translation);
