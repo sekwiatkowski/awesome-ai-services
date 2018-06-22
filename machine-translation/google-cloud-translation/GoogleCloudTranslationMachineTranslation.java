272b8ec1a61d95ceb3b2e3619015a84b379471ce
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-22 22:58:18 +0200

    Added Java sample code for Google Cloud Translation

---
+++ machine-translation/google-cloud-translation/GoogleCloudTranslationMachineTranslation.java
@@ -0,0 +1,12 @@
+// "input" is a string holding the source document.
+// To translate from English to German, set "sourceLanguageCode" to "en" and "targetLanguageCode" to "de".
+
+Translate translate = TranslateOptions.getDefaultInstance().getService();
+
+Translation translation = translate.translate(
+    input,
+    Translate.TranslateOption.format("text"),
+        Translate.TranslateOption.sourceLanguage(sourceLanguage),
+    Translate.TranslateOption.targetLanguage(targetLanguage));
+
+String translatedText = translation.getTranslatedText();
