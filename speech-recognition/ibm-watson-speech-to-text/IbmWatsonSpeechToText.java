beb27efdab15ff46a450b5a90a0523c67af92db2
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-23 05:22:59 +0200

    Added a comment about the input in the Java example of speech recognition with Watson

--- speech-recognition/ibm-watson-speech-to-text/IbmWatsonSpeechToText.java
+++ speech-recognition/ibm-watson-speech-to-text/IbmWatsonSpeechToText.java
@@ -1,7 +1,9 @@
+// "audio" can be a File or an InputStream object
+
 SpeechToText client = new SpeechToText(username, password);
 
 RecognizeOptions options = new RecognizeOptions.Builder()
-    .audio(inputFile)
+    .audio(audio)
     .contentType(HttpMediaType.AUDIO_MP3)
     .build();
 
