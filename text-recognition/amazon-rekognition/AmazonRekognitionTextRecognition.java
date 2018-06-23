4b97730c46ba9d9a3312c5924a55a1175494efd5
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-23 06:17:00 +0200

    Fixed the variable name in the comment

--- text-recognition/amazon-rekognition/AmazonRekognitionTextRecognition.java
+++ text-recognition/amazon-rekognition/AmazonRekognitionTextRecognition.java
@@ -1,4 +1,4 @@
-// "image" is a string holding the path to the image file.
+// "input" is a string holding the path to the image file.
 
 Path path = Paths.get(input);
 byte[] data = Files.readAllBytes(path);
