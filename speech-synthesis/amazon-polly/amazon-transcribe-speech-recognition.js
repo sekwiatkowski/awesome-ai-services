3b6c85bdb6dc1e15516752d131b5d0bee46fe1d9
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-24 01:44:04 +0200

    Added JavaScript code for speech recognition with Amazon Transcribe

---
+++ speech-synthesis/amazon-polly/amazon-transcribe-speech-recognition.js
@@ -0,0 +1,38 @@
+// "jobName" is a self-descriptive string.
+// For speech in US English, set "languageCode" to "en-US".
+// "s3Uri" is a string pointing to an audio file stored in an S3 bucket.
+
+const AWS = require('AWS-sdk')
+
+const transcribeService = new AWS.TranscribeService({
+    region: 'us-east-1'
+})
+
+const params = {
+    LanguageCode: languageCode,
+    Media: {
+        MediaFileUri: s3Uri
+    },
+    MediaFormat: 'mp3',
+    TranscriptionJobName: jobName
+}
+
+const repeatUntilCompleted = (cb) => {
+    transcribeService.getTranscriptionJob({ TranscriptionJobName: jobName }, (getErr, getRes) => {
+        const job = getRes.TranscriptionJob
+        const status = job.TranscriptionJobStatus
+
+        if (status === 'COMPLETED') {
+            cb(job.Transcript.TranscriptFileUri)
+        }
+        else if (status === 'IN_PROGRESS') {
+            setTimeout(repeatUntilCompleted, 1000)
+        }
+    })
+}
+
+transcribeService.startTranscriptionJob(params, (startErr, startRes) => {
+    repeatUntilCompleted(transcriptFileUri => {
+        // do something with the transcript
+    })
+})
