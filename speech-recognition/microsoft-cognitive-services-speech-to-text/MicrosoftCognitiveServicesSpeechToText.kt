c4a845d17b85a9be1b0ee305586741e9c7bfb6a7
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-23 05:34:28 +0200

    Added Kotlin code for speech recognition with MS Cognitive Services Speech

---
+++ speech-recognition/microsoft-cognitive-services-speech-to-text/MicrosoftCognitiveServicesSpeechToText.kt
@@ -0,0 +1,24 @@
+// "input" is a File pointing to an audio file.
+// For US English, set "languageCode" to "en-US".
+
+val bytes = inputFile.readBytes()
+
+val tokenHost = "https://westus.api.cognitive.microsoft.com"
+val tokenPath = "/sts/v1.0/issueToken"
+val tokenUrl = tokenHost + tokenPath
+
+val token = Unirest.post(tokenUrl)
+    .header("Ocp-Apim-Subscription-Key", subscriptionKey)
+    .asString()
+    .body
+
+val sttHost = "https://westus.stt.speech.microsoft.com"
+val sstPath = "/speech/recognition/conversation/cognitiveservices/v1?language=$languageCode"
+val sstUrl = sttHost + sstPath
+
+val response = Unirest.post(sstUrl)
+    .header("Content-Type", "application/json")
+    .header("Authorization", token)
+    .body(bytes)
+    .asString()
+    .body
