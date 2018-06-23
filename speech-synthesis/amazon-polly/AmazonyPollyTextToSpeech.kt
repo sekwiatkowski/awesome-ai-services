0ec2d60f2efac15277591f4db9a552c9d8ab5b05
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-23 05:48:43 +0200

    Added Kotlin code for speech synthesis with Amazon Polly

---
+++ speech-synthesis/amazon-polly/AmazonyPollyTextToSpeech.kt
@@ -0,0 +1,25 @@
+// "ssml" is the input provided in the Speech Synthesis Markup Language.
+// Alternatively, you can provide raw text and set the text type to "TextType.Text".
+
+val client = AmazonPollyClientBuilder
+    .standard()
+    .withCredentials(credentialsProvider)
+    .withRegion(Regions.US_EAST_1)
+    .build()
+
+val request = SynthesizeSpeechRequest()
+    .withText(ssml)
+    .withTextType(TextType.Ssml)
+    .withVoiceId(VoiceId.Matthew)
+    .withOutputFormat(OutputFormat.Mp3)
+
+val result = client.synthesizeSpeech(request)
+
+val audioStream = result.audioStream
+
+val fileOutputStream = FileOutputStream(outputFile)
+
+IOUtils.copy(audioStream, fileOutputStream)
+
+audioStream.close()
+fileOutputStream.close()
