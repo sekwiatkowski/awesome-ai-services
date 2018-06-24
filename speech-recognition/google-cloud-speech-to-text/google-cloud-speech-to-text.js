e63a4a78c4ed052c5e1031bdac98383e42fe507c
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-24 05:49:27 +0200

    Added Node code to recognize speech using Google Cloud STT

---
+++ speech-recognition/google-cloud-speech-to-text/google-cloud-speech-to-text.js
@@ -0,0 +1,31 @@
+// "input" is a string holding the path to the input file (a local FLAC file, in this example).
+// For US English, set "languageCode" to "en-US".
+
+const speech = require('@google-cloud/speech')
+const fs = require('fs');
+
+const client = new speech.SpeechClient()
+
+const file = fs.readFileSync(input
+const audioBytes = file.toString('base64')
+
+const audio = {
+    content: audioBytes
+}
+
+const config = {
+    encoding: 'FLAC',
+    languageCode
+}
+
+const req = { audio, config }
+
+client
+    .recognize(req)
+    .then(res => {
+        const transcription = res[0].results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n')
+    })
+    .catch(err => {
+    })
