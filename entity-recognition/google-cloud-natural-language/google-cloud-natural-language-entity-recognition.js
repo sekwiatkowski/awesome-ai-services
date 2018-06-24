fdb7fa2bc571c0d8170b198b299902aa1f4ab32a
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@googlemail.com>
2018-06-24 04:27:45 +0200

    Added Node code for entity recognition w/ Google Cloud NL

---
+++ entity-recognition/google-cloud-natural-language/google-cloud-natural-language-entity-recognition.js
@@ -0,0 +1,17 @@
+// "content" is a string holding the input document.
+
+const language = require('@google-cloud/language')
+
+const client = new language.LanguageServiceClient()
+
+const document = {
+    content: content,
+    type: 'PLAIN_TEXT'
+}
+
+client.analyzeEntities({ document })
+    .then(results => {
+        const entities = results[0].entities
+    })
+    .catch(err => {
+    })
